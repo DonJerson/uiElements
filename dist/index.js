46e10f0f7dc243536a55b0c05df9d2baa8d65aa2
prieto <jersonamendez@gmail.com>
2022-05-08 19:53:06 -0400

    freshPublished

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,24 @@
 "use strict";
 
-var _react = require("react");
+var _react = _interopRequireDefault(require("react"));
 
-var _client = require("react-dom/client");
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+require("./PrietoLogin/media/css/styles.css");
 
 var _PrietoLogin = _interopRequireDefault(require("./PrietoLogin/PrietoLogin"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var rootElement = document.getElementById("root");
-var root = (0, _client.createRoot)(rootElement);
-root.render( /*#__PURE__*/React.createElement(_react.StrictMode, null, /*#__PURE__*/React.createElement(_PrietoLogin.default, null)));
+// import reportWebVitals from './reportWebVitals';
+_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_PrietoLogin.default, null)), document.getElementById('root')); // If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+// root.render(
+//   <StrictMode>
+//     <PrietoLogin />
+//   </StrictMode>
+// );
